d5fe6e85a7919a8e5290d471975bfa4833122578
Baqiatus Sholeha <baqiatussholeha80@gmail.com>
2022-10-29 20:07:54 +0700

    belajar php 1

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    echo "Hello Word <br>";
+
+    $nama = 'Baqiatus S';
+    $umur = 20;
+    
+    echo "Nama saya <strong> $nama </strong>, usia $umur tahun. <br>";
+
+    $namaAdik = 'Adila';
+    $umurAdik = 12;
+
+    echo "Nama idol saya <strong> $namaAdik </strong>. usia $umurAdik tahun. <br>";
+
+    $selisihUmur = $umur % $umurAdik;
+    echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun."
+?>
